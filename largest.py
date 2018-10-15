ae1ea46f0650d468c2f1a0c10e2c6e17cbf9d113
Kriti22251 <44095493+Kriti22251@users.noreply.github.com>
2018-10-16 00:00:38 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,9 @@
+a=int(input("enter 1st element"))
+b=int(input("enter 2nd element"))
+c=int(input("enter 3rd element"))
+if a>b and b>c:
+    print(a)
+elif b>a and b>c:
+    print(b)
+else:
+    print(c)
