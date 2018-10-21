5aa00d42c84933a447d03ee7f1a646fc647e906e
Kriti22251 <44095493+Kriti22251@users.noreply.github.com>
2018-10-21 16:33:00 +0530

    Add files via upload

---
+++ sortBYQUIck1.py
@@ -0,0 +1,20 @@
+def quicksort(x):
+    if len(x) == 1 or len(x) == 0:
+        return x
+    else:
+        pivot = x[0]
+        i = 0
+        for j in range(len(x)-1):
+            if x[j+1] < pivot:
+                x[j+1],x[i+1] = x[i+1], x[j+1]
+                i += 1
+        x[0],x[i] = x[i],x[0]
+        first = quicksort(x[:i])
+        second = quicksort(x[i+1:])
+        first.append(x[i])
+        return first + second
+
+N=int(input()       
+a = [int(y) for y in input().split()]
+quicksort(a)
+print(a)
