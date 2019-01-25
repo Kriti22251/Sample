6a694efd2833ebf1efacb0c4a28805ccfc331059
Kriti22251 <44095493+Kriti22251@users.noreply.github.com>
2019-01-25 21:58:49 +0530

    Add files via upload

---
+++ MultipleTen.py
@@ -0,0 +1,14 @@
+a1=int(input())
+a = (a1 // 10) * 10
+b = a + 10
+if (a1>=0 and a1<=5):
+    print("10")
+else:
+    if((a1-a)>(b-a1)):
+        print(b)
+    else:
+        print(a)
+      
+   
+   
+    
