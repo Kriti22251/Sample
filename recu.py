dfbf1ad961e2813effcffdca6c328d99bd13382e
Kriti22251 <44095493+Kriti22251@users.noreply.github.com>
2019-06-08 13:45:45 +0530

    Add files via upload

---
+++ recu.py
@@ -0,0 +1,15 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+n=int(input())
+def mul(n):
+    c=n//2
+    mul(c)
+    return c
+    
+print(mul(n))
